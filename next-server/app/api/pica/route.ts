952a4c9f836e1764a754a2f82c63fa493b3a7f20
Charlie Cheesman <djnr.cheesman@gmail.com>
2025-02-23 21:43:39 +0000

    add pica (not setup yet)

---
+++ next-server/app/api/pica/route.ts
@@ -0,0 +1,27 @@
+import { openai } from "@ai-sdk/openai";
+import { generateText } from "ai";
+import { Pica } from "@picahq/ai";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const pica = new Pica(process.env.PICA_SECRET_KEY!);
+
+async function execute(message: string): Promise<string> {
+  const system = await pica.generateSystemPrompt();
+
+  const { text } = await generateText({
+    model: openai("gpt-4o"),
+    system,
+    prompt: message,
+    tools: { ...pica.oneTool },
+    maxSteps: 10,
+  });
+
+  return text;
+}
+
+execute("Star the repo picahq/pica with github")
+  .then((text) => {
+    console.log(text);
+  })
+  .catch(console.error);
